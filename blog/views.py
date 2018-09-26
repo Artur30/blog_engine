20ac5e134638308818913b854c3de13ed0cf3763
Arthur Gumirov <murza@MacBook-Air-Arthur.local>
2018-09-26 23:21:07 +0300

    перееисал с использованием миксинов

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Tag
 from django.views.generic import View
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
@@ -8,18 +9,9 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-"""
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
-"""
-
-
-class PostDetail(View):
-    def get(self, request, slug):
-        post = Post.objects.get(slug__iexact=slug)
-        return render(request, 'blog/post_detail.html', context={'post': post})
-
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
 
 
 def tags_list(request):
@@ -27,7 +19,8 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
 
