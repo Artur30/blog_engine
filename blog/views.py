24cd2f43a39c89de42be2028465a1ffb00fa929a
Arthur Gumirov <murza@MacBook-Air-Arthur.local>
2018-09-27 12:02:30 +0300

    добавил комменты

--- blog/views.py
+++ blog/views.py
@@ -7,27 +7,32 @@ from .forms import TagForm, PostForm
 
 # Контроллеры постов
 class PostsList(ObjectListMixin, View):
+    """ Обрабатывает список постов """
     model = Post
     template = 'blog/index.html'
 
 
 class PostDetail(ObjectDetailMixin, View):
+    """ Обрабатывает отображение отдельного поста """
     model = Post
     template = 'blog/post_detail.html'
 
 
 class PostCreate(ObjectCreateMixin, View):
+    """ Обрабатывает создание поста """
     form = PostForm
     template = 'blog/post_create_form.html'
 
 
 class PostUpdate(ObjectUpdateMixin, View):
+    """ Обрабатывает редактирование поста """
     model = Post
     form = PostForm
     template = 'blog/post_update_form.html'
 
 
 class PostDelete(ObjectDeleteMixin, View):
+    """ Обрабатывает удаление поста """
     model = Post
     redirect_url = 'posts_list_url'
     template = 'blog/post_delete_form.html'
